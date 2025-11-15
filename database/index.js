75cba944ee7c8bd0a20b0a3ece8511e2479b5375
Afcyare <fabdim7@gmail.com>
2025-11-15 11:43:51 +0300

    update

--- database/index.js
+++ database/index.js
@@ -3,39 +3,29 @@ require("dotenv").config();
 
 /* ***************
  * Connection Pool
- * SSL Object needed for local testing of app
- * But will cause problems in production environment
- * If - else will make determination which to use
+ * SSL Object needed for Render Postgres
+ * But not needed for local Postgres
+ * Determines automatically which to use
  * *************** */
 
-let pool;
+// Determine if connecting to Render database
+const isRender = process.env.DATABASE_URL.includes("render.com");
 
-if (process.env.NODE_ENV === "development") {
-  pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  });
+let pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: isRender ? { rejectUnauthorized: false } : false,
+});
 
-  // Added for troubleshooting queries
-  // during development
-
-  module.exports = {
-    async query(text, params) {
-      try {
-        const res = await pool.query(text, params);
-        console.log("executed query", { text });
-        return res;
-      } catch (error) {
-        console.error("error in query", { text });
-        throw error;
-      }
-    },
-  };
-} else {
-  pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-  });
-  module.exports = pool;
-}
+// Added for troubleshooting queries during development
+module.exports = {
+  async query(text, params) {
+    try {
+      const res = await pool.query(text, params);
+      console.log("executed query", { text });
+      return res;
+    } catch (error) {
+      console.error("error in query", { text, error });
+      throw error;
+    }
+  },
+};
